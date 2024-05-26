c895e75c5293179c9416335365efffff08dbbb32
Mekun-k <akaidonnbei@gmail.com>
2024-05-26 13:52:42 +0900

    Add: array_map

--- 03_js_basic/047_array_method/start/main.js
+++ 03_js_basic/047_array_method/start/main.js
@@ -1,2 +1,9 @@
 const arry = [10, 20, 30, 40];
 const newArry = [];
+
+const newArry2 = arry.map(val => val * 2).filter(val => val > 50);
+console.log(newArry2);
+
+// rubyの場合    arry.map { |val| val * 2 }
+// JSの場合      arry.map(val => val * 2)
+
